1c78713c3b787fa4ccb64244734ed7796841dddf
HuyMangekyo <Huyquang.nhht@gmail.com>
2022-10-24 08:56:33 +0700

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let age_change = document.getElementById("change");
+age_change.addEventListener("click",change_age);
+function change_age(){
+
+    document.getElementById("age").innerHTML=20
+}
+// let age_change = document.getElementById("change");
+// age_change.addEventListener("click",change_age);
+// function change_age() {
+//
+//     document.getElementById("age").innerHTML=20;
+// }
+
+
